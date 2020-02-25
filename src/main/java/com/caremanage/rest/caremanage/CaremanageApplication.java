b7a3eff63c67836cbd4507fa1e9fe3222e70692b
Barnabas Ebadan <barnabas2e@gmail.com>
2020-02-24 18:36:30 -0800

    initial commit

---
+++ src/main/java/com/caremanage/rest/caremanage/CaremanageApplication.java
@@ -0,0 +1,13 @@
+package com.caremanage.rest.caremanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaremanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaremanageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caremanage/rest/caremanage/CaremanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caremanage.rest.caremanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaremanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
